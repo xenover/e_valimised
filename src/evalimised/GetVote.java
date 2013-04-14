506dc5e00a47a86c8f46088548b5fc2abcd08e75
Kert Pjatkin <kertp@hotmail.com>
2013-04-14 15:15:35 +0300

    Parandasin natuke hääletamise lehete. Hakkan uurima Channel APIt

--- src/evalimised/GetVote.java
+++ src/evalimised/GetVote.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.channel.ChannelService;
+import com.google.appengine.api.channel.ChannelServiceFactory;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -57,3 +59,21 @@ public class GetVote extends HttpServlet {
 	}
 	
 }
+
+//public class GetVote extends HttpServlet {
+//	  @Override
+//	  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//
+//		    ChannelService channelService = ChannelServiceFactory.getChannelService();
+//
+//		    // The 'Game' object exposes a method which creates a unique string based on the game's key
+//		    // and the user's id.
+//		    String token = channelService.createChannel("xyz");
+//
+//		    // Index is the contents of our index.html resource, details omitted for brevity.
+//		    index = index.replaceAll("\\{\\{ token \\}\\}", token);
+//
+//		    resp.setContentType("text/html");
+//		    resp.getWriter().write(index);
+//	  }
+//}
--- war/scripts/menyySkript.js
+++ war/scripts/menyySkript.js
@@ -5,25 +5,36 @@ function sisu(nimi) {
   {
       setTimeout(function(){document.getElementById("statistika_tekst").style.visibility="hidden";},1500);
       setTimeout(function(){document.getElementById("loader").style.visibility="hidden";},1500);
-      setTimeout(function(){document.getElementById("chart_pie").style.visibility="visible";},1500);
-      setTimeout(function(){document.getElementById("chart_div").style.visibility="visible";},1500);
+      setTimeout(function(){$(".statistika").css( "display", "block" );},1500);
+//      setTimeout(function(){document.getElementById("chart_div").style.visibility="visible";},1500);
   }
   else if( nimi == "haaletamine")
   {
-	  	voteInfo = "Sa pole veel kellelegi oma häält andnud.";
-		var params = "?id=6" 
-		var url = "/GetVote" + params;
-	    $.get(url ,function(responseJson) {
-	      if(responseJson!=null){
+
+      setTimeout(function(){document.getElementById("ootamistekst").style.visibility="hidden";},1500);
+      setTimeout(function(){document.getElementById("loader_haaletamine").style.visibility="hidden";},1500);
+      setTimeout(function(){$(".statistika").css( "display", "block" );},1500);
+	  var params = "?id=6" 
+	  var url = "/GetVote" + params;
+	  $.get(url ,function(responseJson) {
+	      if(responseJson!=""){
 	    	$("#candidatetable").find("tr:gt(0)").remove();
 	        var table1 = $("#candidatetable");
+	        $("#haaletamata").css( "display", "none" );
+	        $("#tyhista").css( "display", "block" );
+	        $("#haaletanud").css( "display", "block" );
 		    $.each(responseJson, function(key,value) { 
-		      voteInfo = "Oled andnud oma hääle juba kandidaadile " +  value['first_name'] + " " + value["last_name"] + ". Uue hääle andmisel eelmine hääl tühistatakse.";
-		      $("#tyhista").css( "display", "block" );
+		      var voteInfo = "Oled andnud oma hääle juba kandidaadile " +  value['first_name'] + " " + value["last_name"] + ". Uue hääle andmisel eelmine hääl tühistatakse.";
+		      $("#haaletanud").append(voteInfo);
 		    });
 	      }
-	      $("#haaletus").append(voteInfo);
+	      else{
+		      $("#haaletamata").css( "display", "block" );
+		      $("#tyhista").css( "display", "none" );
+		      $("#haaletanud").css( "display", "none" );
+	      }
 	    });
+//	  checkVote();
   }
 }
 
@@ -39,3 +50,43 @@ function navig() {
 }
 window.onload = navig;
 window.onhashchange = navig; 
+
+function checkVote()
+{
+  	voteInfo = "Sa pole veel kellelegi oma häält andnud.";
+	var params = "?id=6" 
+	var url = "/GetVote" + params;
+    $.get(url ,function(responseJson) {
+      if(responseJson!=null){
+    	$("#candidatetable").find("tr:gt(0)").remove();
+        var table1 = $("#candidatetable");
+	    $.each(responseJson, function(key,value) { 
+	      voteInfo = "Oled andnud oma hääle juba kandidaadile " +  value['first_name'] + " " + value["last_name"] + ". Uue hääle andmisel eelmine hääl tühistatakse.";
+	      $("#tyhista").css( "display", "block" );
+	    });
+      }
+      $("#haaletus").append(voteInfo);
+    });
+//    channel = new goog.appengine.Channel('{{ token }}');
+//    socket = channel.open();
+//    socket.onopen = onOpened;
+//    socket.onmessage = onMessage;
+//    socket.onerror = onError;
+//    socket.onclose = onClose;
+}
+
+function onOpened() {
+    alert("Channel opened!");
+}
+
+function onMessage(msg) {
+    alert(msg.data);
+}
+
+function onMessage(err) {
+    alert(err);
+}
+
+function onClose() {
+    alert("Channel closed!");
+}
