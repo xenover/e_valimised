8c56d7c57de551d2a5f33e82c9b5a331feb1d5bb
Kert Pjatkin <kertp@hotmail.com>
2013-04-22 10:31:18 +0300

    V etapp eelne

--- war/scripts/login.js
+++ war/scripts/login.js
@@ -1,68 +1,82 @@
 $(document).ready(function() {
+	$.noConflict();
 	var uid;
-// Additional JS functions here
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : 	323774344414844, // App ID
-      channelUrl : '//xenover.appspot.com/channel.html', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
+//	Additional JS functions here
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : 	323774344414844, // App ID
+			channelUrl : '//test.xenover.appspot.com/channel.html', // Channel File
+			status     : true, // check login status
+			cookie     : true, // enable cookies to allow the server to access the session
+			xfbml      : true  // parse XFBML
+		});
+		FB.XFBML.parse();
 
-    FB.getLoginStatus(function(response) {
-    	  if (response.status === 'connected') {
-    		console.log("Oled sisse loginud");
-        	uid = response.authResponse.userID;
-        	checkUser();
-    	  }
-    	  else {
-    		  console.log("pole sisse loginud");
-    	  }
-    	 });
-   
-    function login() {
-        FB.login(function(response) {
-            if (response.authResponse) {
-            	$("#user").html(response.name);
-            	
-            } else {
-            	$("#user").html();
-            }
-        });
-    }
-    
-    function checkUser(){
-    	FB.api(
-    			{
-    				method: 'fql.query',
-    				query: 'SELECT uid, first_name, last_name FROM user WHERE uid = ' + uid
-    			},
-    			function(resp) {
-    				//    do something with the response
-    				console.log(resp[0].uid);
-    				// KIRUTADA JUURDE, KAS KASUTAJA JUBA ANDMEBAASIS OLEMAS
-    				var params = "?";
-    				params += "id=" + resp[0].uid; 
-    				var url = "/SearchPerson" + params;
-    				$.get(url ,function(response) {
-    					if(response == 0)
-    					{
-    						newUser(resp[0].uid);
-    					}
-    				});
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				console.log("Oled sisse loginud");
+				uid = response.authResponse.userID;
+				checkUser();
+			}
+			else {
+				console.log("pole sisse loginud");
+				localStorage.setItem('newuser',false);
+			}
+		});
 
-    			}
-    	);
-    }
-  };
+		FB.Event.subscribe('auth.login', function(response) {
+			// do something with response
+			uid = response.authResponse.userID;
+			checkUser();
+			alert("Login sisse");
+			location.reload();
+		});
+		FB.Event.subscribe('auth.logout', function(response) {
+			// do something with response
+			localStorage.setItem('newuser',false);
+			alert("Login välja");
+			location.reload();
+		});
+	};
 
-  // Load the SDK Asynchronously
-  (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
-   }(document));
+	function checkUser(){
+		FB.api(
+				FB.api('/fql', 'GET', {q:'SELECT uid, first_name, last_name FROM user WHERE uid = ' + uid}, function(response){
+					  if (response && response.data)
+					    console.log(response.data);
+						// KIRUTADA JUURDE, KAS KASUTAJA JUBA ANDMEBAASIS OLEMAS
+						var params = "?";
+						params += "id=" + resp[0].uid; 
+						var url = "/SearchPerson" + params;
+						$.get(url ,function(response) {
+							if(response == 0)
+							{
+								(function($) {
+									console.log("TEST");
+									localStorage.setItem('newuser',true);
+									})(jQuery);
+								newUser(resp[0]);
+							}
+						});
+					});
+		)};
+
+	// Load the SDK Asynchronously
+	(function(d){
+		var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement('script'); js.id = id; js.async = true;
+		js.src = "//connect.facebook.net/en_US/all.js";
+		ref.parentNode.insertBefore(js, ref);
+	}(document));
 });
+
+// Uue kasutaja loomiseks
+function newUser(resp){
+	console.log("TESTIN");
+	sisu("uuskasutaja");
+	$("#eesnimi").append(resp.first_name);
+	console.log(resp.first_name);
+	$("#perenimi").append(resp.last_name);
+	console.log(resp.last_name);
+};
--- war/scripts/menyySkript.js
+++ war/scripts/menyySkript.js
@@ -1,38 +1,45 @@
 function sisu(nimi) {
-  $("#keha").empty();
-  $("#keha").load(nimi + ".html")
-  if(nimi == "statistika")
-  {
-      setTimeout(function(){document.getElementById("statistika_tekst").style.visibility="hidden";},1500);
-      setTimeout(function(){document.getElementById("loader").style.visibility="hidden";},1500);
-      setTimeout(function(){$(".statistika").css( "display", "block" );},1500);
-//      setTimeout(function(){document.getElementById("chart_div").style.visibility="visible";},1500);
+	jQuery("#keha").empty();
+  
+  localStorage.getItem('newuser');
+  if(localStorage.getItem('newuser') == "true" ){
+	  jQuery("#keha").load("uuskasutaja.html");
   }
-  else if( nimi == "haaletamine")
-  {
-      setTimeout(function(){document.getElementById("ootamistekst").style.visibility="hidden";},1500);
-      setTimeout(function(){document.getElementById("loader_haaletamine").style.visibility="hidden";},1500);
-      setTimeout(function(){$(".statistika").css( "display", "block" );},1500);
-	  var params = "?id=6" 
-	  var url = "/GetVote" + params;
-	  $.get(url ,function(responseJson) {
-	      if(responseJson!=""){
-	    	$("#candidatetable").find("tr:gt(0)").remove();
-	        var table1 = $("#candidatetable");
-	        $("#haaletamata").css( "display", "none" );
-	        $("#tyhista").css( "display", "block" );
-	        $("#haaletanud").css( "display", "block" );
-		    $.each(responseJson, function(key,value) { 
-		      var voteInfo = "Oled andnud oma hääle juba kandidaadile " +  value['first_name'] + " " + value["last_name"] + ". Uue hääle andmisel eelmine hääl tühistatakse.";
-		      $("#haaletanud").append(voteInfo);
-		    });
-	      }
-	      else{
-		      $("#haaletamata").css( "display", "block" );
-		      $("#tyhista").css( "display", "none" );
-		      $("#haaletanud").css( "display", "none" );
-	      }
-	    });
+  else{
+	  jQuery("#keha").load(nimi + ".html")
+	  if(nimi == "statistika")
+	  {
+		  setTimeout(function(){document.getElementById("statistika_tekst").style.visibility="hidden";},1500);
+		  setTimeout(function(){document.getElementById("loader").style.visibility="hidden";},1500);
+		  setTimeout(function(){jQuery(".statistika").css( "display", "block" );},1500);
+//		  setTimeout(function(){document.getElementById("chart_div").style.visibility="visible";},1500);
+	  }
+	  else if( nimi == "haaletamine")
+	  {
+		  setTimeout(function(){document.getElementById("ootamistekst").style.visibility="hidden";},1500);
+		  setTimeout(function(){document.getElementById("loader_haaletamine").style.visibility="hidden";},1500);
+		  setTimeout(function(){jQuery(".statistika").css( "display", "block" );},1500);
+		  var params = "?id=6" 
+			  var url = "/GetVote" + params;
+		  	jQuery.get(url ,function(responseJson) {
+			  if(responseJson!=""){
+				  jQuery("#candidatetable").find("tr:gt(0)").remove();
+				  var table1 = jQuery("#candidatetable");
+				  jQuery("#haaletamata").css( "display", "none" );
+				  jQuery("#tyhista").css( "display", "block" );
+				  jQuery("#haaletanud").css( "display", "block" );
+				  jQuery.each(responseJson, function(key,value) { 
+					  var voteInfo = "Oled andnud oma hääle juba kandidaadile " +  value['first_name'] + " " + value["last_name"] + ". Uue hääle andmisel eelmine hääl tühistatakse.";
+					  jQuery("#haaletanud").append(voteInfo);
+				  });
+			  }
+			  else{
+				  jQuery("#haaletamata").css( "display", "block" );
+				  jQuery("#tyhista").css( "display", "none" );
+				  jQuery("#haaletanud").css( "display", "none" );
+			  }
+		  });
+	  }
   }
 }
 
@@ -54,16 +61,16 @@ function checkVote()
   	voteInfo = "Sa pole veel kellelegi oma häält andnud.";
 	var params = "?id=6" 
 	var url = "/GetVote" + params;
-    $.get(url ,function(responseJson) {
+    jQuery.get(url ,function(responseJson) {
       if(responseJson!=null){
-    	$("#candidatetable").find("tr:gt(0)").remove();
-        var table1 = $("#candidatetable");
-	    $.each(responseJson, function(key,value) { 
+    	jQuery("#candidatetable").find("tr:gt(0)").remove();
+        var table1 = jQuery("#candidatetable");
+	    jQuery.each(responseJson, function(key,value) { 
 	      voteInfo = "Oled andnud oma hääle juba kandidaadile " +  value['first_name'] + " " + value["last_name"] + ". Uue hääle andmisel eelmine hääl tühistatakse.";
-	      $("#tyhista").css( "display", "block" );
+	      jQuery("#tyhista").css( "display", "block" );
 	    });
       }
-      $("#haaletus").append(voteInfo);
+      jQuery("#haaletus").append(voteInfo);
     });
 }
 
